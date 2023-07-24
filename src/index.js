7755e644aa3926a3acb3f77d1a7f1efd9484841a
Bahman Akbari <vahoovahoo@gmail.com>
2023-07-24 09:28:19 +0330

    Add basename prop to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
@@ -6,13 +6,13 @@ import App from "./App";
 
 import "./index.css";
 import "react-confirm-alert/src/react-confirm-alert.css";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <StrictMode>
+    <BrowserRouter basename="contact-manager-app">
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </StrictMode>
 );
